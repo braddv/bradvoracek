c80a16c95ba69638be20ac15ef4a4c1521dd2db3
Bradley Voracek <brad.dv@gmail.com>
2016-09-24 11:51:06 -0700

    Add new navbar for projects

--- routes/projects.js
+++ routes/projects.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('projects', { title: 'Brad Voracek' });
 });
 
+router.get('/drones', function(req, res, next) {
+  res.render('projects/drones', { title: 'Brad Voracek' });
+});
+
+router.get('/marblerun', function(req, res, next) {
+  res.render('projects/marblerun', { title: 'Brad Voracek' });
+});
+
 module.exports = router;
