818ba4781a96586cbd50727b876800f648c88113
Bradley Voracek <brad.dv@gmail.com>
2016-09-24 17:16:17 -0700

    add garden page and comply w/ postimg terms of use

--- routes/projects.js
+++ routes/projects.js
@@ -14,4 +14,8 @@ router.get('/marblerun', function(req, res, next) {
   res.render('projects/marblerun', { title: 'Brad Voracek' });
 });
 
+router.get('/garden', function(req, res, next) {
+  res.render('projects/garden', { title: 'Brad Voracek' });
+});
+
 module.exports = router;
